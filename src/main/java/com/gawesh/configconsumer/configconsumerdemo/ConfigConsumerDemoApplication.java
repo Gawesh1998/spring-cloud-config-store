fcc1ba4c74ad5ed81b6ece5235b50a961f1ed8c8
Gawesh1998 <gawesh2020java@gmail.com>
2022-07-17 19:59:16 +0530

    config-consumer

---
+++ src/main/java/com/gawesh/configconsumer/configconsumerdemo/ConfigConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gawesh.configconsumer.configconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigConsumerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigConsumerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gawesh/configconsumer/configconsumerdemo/ConfigConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gawesh.configconsumer.configconsumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
